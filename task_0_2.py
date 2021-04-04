8e75ac5a036ab3340efc7d37d0ed419cbce9aca6
Shalom91 <shalom.mothapo@gmail.com>
2021-04-04 21:01:49 +0200

    converting pseudocode into executable code task_0_2

---
+++ task_0_2.py
@@ -0,0 +1,12 @@
+x = 1 + 1 * 2
+y = (1 + 1) * 2
+z = 1 + ( 1 * 2 )
+a = 1 + 1 * 2 / 2
+b = (1 + 1 * 2 ) /  2
+
+bodmas_operations = f"x = {x} \n" \
+                    f"x = {y} \n" \
+                    f"z = {z} \n" \
+                    f"a = {a} \n" \
+                    f"b = {b}" 
+print(bodmas_operations)
